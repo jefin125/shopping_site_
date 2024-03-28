0f40cb014e1b4c76f8edde3028de7945391c7108
jefin125 <135200384+jefin125@users.noreply.github.com>
2024-03-28 12:01:38 +0400

    Add files via upload

---
+++ message.js
@@ -0,0 +1,20 @@
+function msg(){
+    alert(" Shirt Added to the cart");
+}
+
+function changecolor(){
+    document.getElementById('btn1').style.backgroundColor = 'green';  
+     
+}
+
+function shop(){
+    alert("New arrivals are coming soon");
+    
+}
+
+function focuseevent()
+            {
+                document.getElementById("input1").style.background="aqua";
+            }
+
+
